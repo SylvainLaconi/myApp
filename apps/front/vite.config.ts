0c3ec2e55d781d6051efd5886e0b18858b08378b
SylvainLaconi <sylvain.laconi@gmail.com>
2022-10-23 22:47:26 +0200

    deploy_front fix

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: process.env.FRONT_PORT || 3001,
-  },
-  plugins: [react()]
-})
+	server: {
+		host: '0.0.0.0',
+		port: Number(process.env.PORT) || 3000,
+	},
+	plugins: [react()],
+});
+
