eb4a45c4f3a19bb34f03b6420c41dd15e7b3a135
SylvainLaconi <sylvain.laconi@gmail.com>
2022-10-24 09:46:08 +0200

    fix VITE_API_URL production

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -9,7 +9,11 @@ function App() {
 		[],
 	);
 
-	console.log('VITE_API_URL', import.meta.env.VITE_API_URL);
+	console.log('MODE', import.meta.env.MODE);
+	console.log('BASE_URL', import.meta.env.BASE_URL);
+	console.log('PROD', import.meta.env.PROD);
+	console.log('DEV', import.meta.env.DEV);
+	console.log('SSR', import.meta.env.SSR);
 
 	useEffect(() => {
 		const fetchData = async () => {
