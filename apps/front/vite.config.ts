9bb35858074959ecd3ec4a64aa74eb845ded0e11
SylvainLaconi <sylvain.laconi@gmail.com>
2022-10-25 09:14:43 +0200

    almost there 3

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,23 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  console.log("env", env);
-  
-  return{
-	define: {
-      __APP_ENV__: env.APP_ENV
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return {
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      "process.env": process.env,
     },
 	server: {
 		host: '0.0.0.0',
-    port: parseInt(process.env.PORT, 10) || 3000,
+    port: parseInt(env.PORT, 10) || 3000,
     },
   preview: {
 		host: '0.0.0.0',
-    port: parseInt(process.env.PORT, 10) || 3000,
+    port: parseInt(env.PORT, 10) || 3000,
     },
-	plugins: [react()],
 }});
 
