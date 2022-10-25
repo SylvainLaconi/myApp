08bc04af79357f030b87978787ce48c9254cf1cc
SylvainLaconi <sylvain.laconi@gmail.com>
2022-10-25 10:40:55 +0200

    vscode restart

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -10,8 +10,6 @@ function App() {
 	);
 
 	console.log('MODE', import.meta.env.MODE);
-	console.log('PROD', import.meta.env.PROD);
-	console.log('DEV', import.meta.env.DEV);
 
 	console.log('VITE_API_URL', import.meta.env.VITE_API_URL);
 
