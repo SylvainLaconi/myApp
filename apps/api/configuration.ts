2a0ab5501a93fcc2ec3959fdb1dce3a07a749a57
Sylvain Laconi <74108455+SylvainLaconi@users.noreply.github.com>
2022-11-25 12:09:12 +0100

    Develop (#27)
    * update app.e2e
    
    * modif test_api.yml
    
    * modif test_api.yml 2
    
    * modif test_api.yml 3
    
    * modif test_api.yml 4
    
    * modif test_api.yml 5
    
    * modif test_api.yml 6
    
    * modif test_api.yml 7
    
    * modif test_api.yml 8
    
    * modif test_api.yml 9
    
    * modif test_api.yml 10
    
    * modif test_api.yml 11
    
    * modif test_api.yml 12
    
    * modif test_api.yml 13
    
    * modif test_api.yml 14
    
    * modif test_api.yml 15
    
    * modif test_api.yml 16
    
    * modif test_api.yml 17
    
    * modif test_api.yml 18
    
    * modif test_api.yml 19
    
    * modif test_api.yml 20
    
    * modif test_api.yml 21
    
    * modif test_api.yml 22
    
    * modif test_api.yml 23
    
    * modif test_api.yml 24
    
    * modif test_api.yml 25
    
    * modif test_api.yml 26
    
    * modif test_api.yml 27
    
    * modif test_api.yml 28
    
    * modif test_api.yml 29
    
    * modif test_api.yml 30
    
    Co-authored-by: Sylvain Laconi <sylvainlaconi@MacBook-Air-de-Sylvain.local>

--- apps/api/configuration.ts
+++ apps/api/configuration.ts
@@ -10,7 +10,7 @@ export const configuration = () => ({
     type: 'postgres',
     url: process.env.POSTGRES_URL || '',
     entities: [`${process.cwd()}/dist/src/**/*.entity{ .ts,.js}`],
-    synchronize: process.env.NODE_ENV === 'development' ? true : true, //Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+    synchronize: process.env.NODE_ENV !== 'production' ? true : true, //Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
     autoLoadEntities: true,
     seeds: [process.env.TYPEORM_SEEDING_SEEDS],
     factories: [process.env.TYPEORM_SEEDING_FACTORIES],
--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,34 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppController } from '../src/app.controller';
-import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  const URL = process.env.API_URL;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [],
-      controllers: [AppController],
-      providers: [ConfigService],
-    }).compile();
+  describe('/ (GET)', () => {
+    it("should return 'API is running ❤ on development'", async () => {
+      const response = await request(URL).get('/');
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+      expect(response.status).toBe(200);
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect(`API is running ❤ on ${process.env.NODE_ENV}`);
-  });
-
-  afterAll(async () => {
-    await app.close();
+      expect(response.text).toEqual('API is running ❤ on development');
+    });
   });
 });
